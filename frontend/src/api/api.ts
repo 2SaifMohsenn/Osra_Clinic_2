afec87b57c207e31f7d6ab0fc31b2967e70177c5
SAIF-MOHSEN <s.mohsen2389@nu.edu.eg>
2025-12-14 16:14:10 +0200

    start in linking front with the backend

---
+++ frontend/src/api/api.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const BASE_URL = "http://10.0.2.2:8000/api";
+// for iOS or web:
+// const BASE_URL = "http://127.0.0.1:8000/api";
+
+const api = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default api;
